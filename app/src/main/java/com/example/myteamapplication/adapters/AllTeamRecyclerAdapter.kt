6daaf9d1546ebdb89f5a76fbce73adc954efda90
Dmytro Bespalenko <bespalenko.dmytro@gmail.com>
2021-04-23 19:05:03 +0300

    recycler

---
+++ app/src/main/java/com/example/myteamapplication/adapters/AllTeamRecyclerAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.myteamapplication.adapters
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myteamapplication.R
+
+class AllTeamRecyclerAdapter(private val allTeams: List<String>) :
+    RecyclerView.Adapter<AllTeamRecyclerAdapter.MyViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.all_teams_card, parent, false)
+
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.numberAllTeam?.text = allTeams[position]
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return allTeams.size
+    }
+
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var numberAllTeam: TextView? = null
+        var cityAllTeam: TextView? = null
+        var activityAllTeam: TextView? = null
+
+
+        init {
+
+            numberAllTeam = itemView.findViewById(R.id.number_all_team_textView)
+            cityAllTeam = itemView.findViewById(R.id.city_all_team_textView)
+            activityAllTeam = itemView.findViewById(R.id.activity_all_team_textView)
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/myteamapplication/allteams/AllTeamsFragment.kt
+++ app/src/main/java/com/example/myteamapplication/allteams/AllTeamsFragment.kt
@@ -3,16 +3,21 @@ package com.example.myteamapplication.allteams
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myteamapplication.R
 import com.example.myteamapplication.base.BasicFragment
 
 class AllTeamsFragment : BasicFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView_all_ream)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+
 
     }
 
